07549adf99c55ec83d4b442268150cef8e1cac19
dylan <dylanjh34@gmail.com>
2022-10-04 15:48:08 +0200

    v3

--- exo_13.rb
+++ exo_13.rb
@@ -1,10 +1,14 @@
 nums = Array.new()
 
 50.times do |i|
+    if i < 9 #9 fois
+    nums.push("dylanjh0#{i+1}@gmail.com")
+    else  #41 fois
     nums.push("dylanjh#{i+1}@gmail.com")
+    end
 end
 
-puts "#{nums}"
+puts nums
 
 #tableau = Array.new(50) {|i| "jean.dupont.#{i + 1}@email.fr"} 
 #puts tableau
--- exo_14.rb
+++ exo_14.rb
@@ -1,12 +1,18 @@
 nums = Array.new()
 
 50.times do |i|
-    nums.push("dylanjh#{i}@gmail.com")
+    if i < 9 #9 fois
+        nums.push("dylanjh0#{i+1}@gmail.com")
+        else  #41 fois
+        nums.push("dylanjh#{i+1}@gmail.com")
+        end
 end
 
-div = nums.length / 2
+div = nums.length
 
 div.times do |i|
-    puts nums[i * 2]
+    if i % 2 != 0
+        puts nums[i]
+    end
 end
 
